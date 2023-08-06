2a6764c1ea3dc9bee9184f21b5ccd9dff3b22aa3
tongari07 <azuha07i@gmail.com>
2023-08-06 19:47:57 +0900

    Setup ESLint and Prettier and pre-commit hooks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,30 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "no-console": "error",
-    "react-refresh/only-export-components": [
-      "warn",
+    'import/default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-unresolved': 'error',
+    'no-console': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
   },
-};
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
+}
