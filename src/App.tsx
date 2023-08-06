52cf2dec60f557da286738ef5eec23fdfe643a02
十河直人 <ntogawa@tougounaotonoMac-mini.local>
2023-08-06 18:16:24 +0900

    Setup Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
+      <h1 className="text-6xl font-bold">Population trends by prefecture</h1>
+    </main>
   )
 }
 
